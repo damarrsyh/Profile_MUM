412a502150e429138949844849b432c6ef2f8b9f
damarrnsyh <ramadiansyahdamar@gmail.com>
2022-11-03 15:57:18 +0700

    commit 03112022

--- app/Http/Middleware/CekLevel.php
+++ app/Http/Middleware/CekLevel.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class Roles
+class CekLevel
 {
     /**
      * Handle an incoming request.
@@ -14,11 +14,12 @@ class Roles
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next, ...$roles)
+    public function handle(Request $request, Closure $next, ...$levels)
     {
-        if(in_array($request->user()->role, $roles)){
-            return $next($request);
-        }
+        if(in_array($request->user()->level, $levels)){
+            return next($request);
+        }    
+
         return redirect('/');
     }
 }
